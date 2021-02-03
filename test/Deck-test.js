a981c814ce683b1848b452968d1de98099f08cb2
Anneke Miers <annekemiers.am@gmail.com>
2021-02-02 22:15:46 -0700

    Refactor deck test with beforeEach function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,19 +6,21 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe ('Deck', function() {
+  let deck;
+
+  beforeEach(function() {
+    deck = new Deck()
+  });
 
   it('should be a function', function() {
-    const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', function() {
-    const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should have a default of zero cards', function() {
-    const deck = new Deck();
     expect(deck.cardList.length).to.equal(0);
   });
 
@@ -28,8 +30,6 @@ describe ('Deck', function() {
     const card3 = new Card(19, 'How many meals are there?', [3, 5, 7], 7);
     const card4 = new Card(10, 'No admittance except...', ['on party business', 'if you speak friend', 'FOR FRODO!'], 'on party business');
     
-    const deck = new Deck();
-
     deck.cardList.unshift(card1);
     deck.cardList.unshift(card2);
     deck.cardList.unshift(card3);
@@ -37,5 +37,4 @@ describe ('Deck', function() {
 
     expect(deck.countCardsInDeck()).to.equal(4);
   });
-
 });
